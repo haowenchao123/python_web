45087b8634e92177d6c89639b0de90d436122fc6
haowenchao <haowenchao123@gmail.com>
2019-08-21 17:22:35 +0800

    url

--- frames/Django/djangosite/app/models.py
+++ frames/Django/djangosite/app/models.py
@@ -15,3 +15,104 @@ class Moment(models.Model):
 	content = models.CharField(max_length = 200)
 	user_name =  models.CharField(max_length = 20)
 	kind = models.CharField(max_length = 20, choices = KIND_CHOICES, default = KIND_CHOICES[0])
+
+LEVELS = (
+	('1', 'Very good'),
+	('2', 'Good'),
+	('3', 'Normal'),
+	('4', 'Bad'),
+)
+
+class Comment(models.Model):
+	id = models.AutoField(primary_key = True)
+	headline = models.CharField(max_length = 255)
+	body_text = models.TextField()
+	pub_date = models.DateField()
+	n_visits = models.IntegerField()
+	level = models.CharField("请为本条信息评数", max_length = 1, choices = LEVELS)
+
+	def __str__(self):
+		return self.headline
+
+'''
+# 抽象类继承
+class MessageBase(models.Model):
+	id = models.AutoField()
+	content = models.CharField(max_length = 100)
+	user_name =  models.CharField(max_length = 80)
+	pub_date = models.DateField()
+
+	class Meta:
+		abstract = True
+
+class Moment(MessageBase):
+	headline = models.CharField(max_length = 50)
+
+class Comment(MessageBase):
+	level = models.CharField(max_length = 1, choices = LEVELS)
+
+'''
+
+'''
+# 多表继承
+class MessageBase(models.Model):
+	id = models.AutoField()
+	content = models.CharField(max_length = 100)
+	user_name =  models.CharField(max_length = 80)
+	pub_date = models.DateField()
+
+class Moment(MessageBase):
+	headline = models.CharField(max_length = 50)
+
+class Comment(MessageBase):
+	level = models.CharField(max_length = 1, choices = LEVELS)
+'''
+
+'''
+# 代理模型继承
+class Comment(models.Model):
+	id = models.AutoField()
+	headline = models.CharField(max_length = 50)
+	content = models.CharField(max_length = 100)
+	user_name =  models.CharField(max_length = 80)
+	pub_date = models.DateField()
+
+class OrderedMoment(Moment):
+	class Meta:
+		proxy = True
+		ordering = ["-pub_date"]
+'''
+
+
+class Account(models.Model):
+	user_name = models.CharField(max_length = 80)
+	password = models.CharField(max_length = 255)
+	reg_date = models.DateField()
+
+	def __unicode__(self):
+		return "Account: %s" % self.user_name
+
+class Contact(models.Model):
+	'''
+	#一对一
+	account = models.OneToOneField(
+		Account,
+		on_delete = models.CASCADE,
+		primary_key=True,
+	)
+	'''
+
+	'''
+	#一对多
+	account = models.ForeignKey(Account, on_delete = models.CASCADE)
+	'''
+
+	# 多对多
+	accounts = models.ManyToManyField(Account)
+	zip_code = models.CharField(max_length = 10)
+	address = models.CharField(max_length = 80)
+	mobile = models.CharField(max_length = 20)
+
+	def __unicode__(self):
+		return "%s, %s" % (self.account.user_name, mobile)
+
--- frames/Django/djangosite/app/urls.py
+++ frames/Django/djangosite/app/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from django.conf.urls import url
 from . import views
 
@@ -5,4 +6,10 @@ urlpatterns = [
 	url(r'moments_input', views.moments_input),
 	url(r'', views.welcome, name='first-url'),
 
+	path(r'year/2018', views.moments_2018),
+	url(r'^year/([0-9]{4})/$', views.year_moments),
+	url(r'^year/([0-9]{4})/([0-9]{2})/$', views.year_moments),
+	url(r'^year/([0-9]{4})/([0-9]{2})/([0-9]+)/$', views.year_moments),
+	url(r'^year/?P<year>([0-9]{4})/$', views.year_moments),
+	url(r'^year/?P<year>([0-9]{4})/?P<month>([0-9]{2})/$', views.year_moments),
 ]

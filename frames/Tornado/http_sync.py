6f3a486a978a9a4e97b306cec19af1b3cf79b30c
haowenchao <haowenchao123@gmail.com>
2019-08-25 11:37:11 +0800

    try the async in Tornado

--- frames/Tornado/http_sync.py
+++ frames/Tornado/http_sync.py
@@ -1,6 +1,15 @@
 from tornado.httpclient import HTTPClient
+from tornado.httpclient import AsyncHTTPClient
 
 def synchronous_visit():
 	http_client = HTTPClient()
 	response = http_client.fetch("www.baidu.com"). #阻塞，直到对www.baidu.com访问完成
 	print(response.body)
+
+
+def handle_response(response):
+	print(response.body)
+
+def asynchronous_visit():
+	http_client = AsyncHTTPClient()
+	http_client.fetch("www.baidu.com", callback = handle_response)
